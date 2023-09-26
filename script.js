43f6e794043317ee46bf8a1c444a4a1b4f87d21e
AlexDolz <aleksejdolznikov3@gmail.com>
2023-09-26 16:34:12 +0200

    constructor functions

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Constructor functions
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // ! Never do this
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. New empty object {} is created
+// 2. Function is called, this ==== {} empty object
+// 3. {} linked to prototype
+// 4. Functin automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); //true
+console.log(jay instanceof Person); // error, there is no const jay
