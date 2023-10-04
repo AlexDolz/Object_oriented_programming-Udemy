9edf26c98ed807dfa6bcf1f89551a441e61d353e
AlexDolz <aleksejdolznikov3@gmail.com>
2023-10-04 17:25:44 +0200

    OOP ES6 classes

--- script.js
+++ script.js
@@ -73,3 +73,41 @@ const h1 = document.querySelector('h1');
 console.log(h1);
 
 console.dir(x => x + 1);
+
+// ****************** ES6 Classes *******************************
+
+// class expression
+// const PersonCl = class {
+
+// }
+// class declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica.__proto__ === PersonCl.prototype); // true
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Class are first-class citizes
+// 3. Classes are executed in strict mode
