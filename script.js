205017d97fbd5211d395e6c09e9be26868e49a3b
AlexDolz <aleksejdolznikov3@gmail.com>
2023-10-06 16:50:11 +0200

    object.create

--- script.js
+++ script.js
@@ -167,3 +167,28 @@ Person.hey = function () {
 };
 
 Person.hey();
+
+// *************************** Object.create ************************
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto); // true
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
